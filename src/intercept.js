acce2e476226afa28c3163bde014d605a6d55cd8
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2022-01-27 21:28:24 +0000

    replaces -place order button
    curtesy of @larsroettig

--- src/intercept.js
+++ src/intercept.js
@@ -1,55 +1,30 @@
-const moduleOverrideWebpackPlugin = require('./moduleOverrideWebpackPlugin');
-const componentOverrideMapping = require('./componentOverrideMapping')
-
 module.exports = targets => {
-    const peregrineTargets = targets.of('@magento/peregrine');
-    const veniaTargets = targets.of('@magento/venia-ui');
-    const buildpackTargets = targets.of('@magento/pwa-buildpack');
-    const talonsTarget = peregrineTargets.talons;
-    const paymentMethods = [
-        'paypal_express'   
-    ];
+    const targetables = Targetables.using(targets);
 
-    targets.of('@magento/pwa-buildpack').specialFeatures.tap(flags => {
-        flags[targets.name] = {esModules: true, cssModules: true, graphqlQueries: true, i18n: true};
-    });
-
-    veniaTargets.routes.tap(
-        routesArray => {
-            routesArray.push({
-                name: 'Checkout Success Page',
-                path: '@watch3r/magento-pwa-paypal/src/components/successPage'
-            });
-
-            return routesArray;
-        });
-
-    talonsTarget.tap(talonWrapperConfig => {
-        talonWrapperConfig.CheckoutPage.useCheckoutPage.wrapWith(
-            '@watch3r/magento-pwa-paypal/src/plugins/checkoutPageTalonPlugin'
-        );
-    });
-
-    talonsTarget.tap(talonWrapperConfig => {
-        talonWrapperConfig.CartPage.useCartPage.wrapWith(
-            '@watch3r/magento-pwa-paypal/src/plugins/cartPageTalonPlugin'
-        );
-    });
-
-    const gatewaysPath = '@watch3r/magento-pwa-paypal/src/components/gateways/'
-    
-    // can add more then one payment method 
-
-    paymentMethods.map((method) =>
-        veniaTargets.checkoutPagePaymentTypes.tap(
-            checkoutPagePaymentTypes => checkoutPagePaymentTypes.add({
-                paymentCode: method,
-                importPath: gatewaysPath + method
-            }),
-        )
+    const CheckoutPageComponent = targetables.reactComponent(
+        '@magento/venia-ui/lib/components/CheckoutPage/checkoutPage.js'
     );
 
-    buildpackTargets.webpackCompiler.tap(compiler => {
-        new moduleOverrideWebpackPlugin(componentOverrideMapping).apply(compiler);
-    }); 
-};
+    const CustomPlaceOrder = CheckoutPageComponent.addImport(
+        "CustomPlaceOrder from '@watch3r/magento-pwa-paypal/src/components/placeOrderButton'"
+    );
+
+    /**
+     * Replace the place order button to the checkout page
+     */
+    const placeOrderButton = '{placeOrderButton}';
+    CheckoutPageComponent.insertBeforeSource(
+        placeOrderButton,
+        '{customPlaceOrderButton}',
+        {
+            remove: placeOrderButton.length
+        }
+    );
+
+    /**
+     * Define the custom place order button
+     */
+    CheckoutPageComponent.insertBeforeSource(
+        'const orderSummary',
+        `const customPlaceOrderButton = checkoutStep === CHECKOUT_STEP.REVIEW ? (<${CustomPlaceOrder} originalPlaceOrderButton={placeOrderButton} handlePlaceOrder={handlePlaceOrder}></${CustomPlaceOrder}>): null;\n`
+    );
