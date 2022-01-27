8b4c07624adbf5ce39badff6bd95933bba88dd72
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2022-01-27 18:09:26 +0000

    Create componentOverrideMapping.js

---
+++ src/componentOverrideMapping.js
@@ -0,0 +1,4 @@
+module.exports = componentOverride = {
+    [`@magento/venia-ui/lib/components/CheckoutPage/PaymentInformation/paymentMethods.js`]:
+        '@watch3r/magento-pwa-paypal/src/override/CheckoutPage/PaymentInformation/paymentMethods.js'
+};
