7c23707404a7a56be07c80fed0a9b00d9469c3cc
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2022-01-27 19:44:42 +0000

    Create usePayPal.js
    start

---
+++ src/talons/usePayPal.js
@@ -0,0 +1,7 @@
+// use pay pal talon 
+import { useCallback, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import mergeOperations from '@magento/peregrine/lib/util/shallowMerge';
+import { useCartContext } from '@magento/peregrine/lib/context/cart';
+
+import DEFAULT_OPERATIONS from './paypal.gql';
