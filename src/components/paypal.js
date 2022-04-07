6cc46044e430047133fa8849f17cc45fc369adeb
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2022-04-07 07:47:40 +0100

    Update paypal.js

--- src/components/paypal.js
+++ src/components/paypal.js
@@ -1,93 +1,349 @@
-import React from 'react';
-import { useStyle } from '@magento/venia-ui/lib/classify';
-import { shape, string, bool, func } from 'prop-types';
-import BillingAddress from '@magento/venia-ui/lib/components/CheckoutPage/BillingAddress';
+// copied from @magento/venia-ui/lib/components/CheckoutPage/PaymentInformation/creditCard.js
+import React, { useMemo, useCallback } from 'react';
+import { useIntl } from 'react-intl';
+import { bool, func, shape, string } from 'prop-types';
+import { useCreditCard } from '../talons/useCreditCard';
 
-import { useCheckmo } from '../talons/useCheckmo';
-import defaultClasses from './checkmo.module.css';
-import { FormattedMessage } from 'react-intl';
+import { isRequired } from '@magento/venia-ui/lib/util/formValidators';
+import Country from '@magento/venia-ui/lib/components/Country';
+import Region from '@magento/venia-ui/lib/components/Region';
+import Postcode from '@magento/venia-ui/lib/components/Postcode';
+import Checkbox from '@magento/venia-ui/lib/components/Checkbox';
+import Field from '@magento/venia-ui/lib/components/Field';
+import TextInput from '@magento/venia-ui/lib/components/TextInput';
+import StripeDropin from './stripeDropin.js';
+import LoadingIndicator from '@magento/venia-ui/lib/components/LoadingIndicator';
+import { useStyle } from '@magento/venia-ui/lib/classify';
+
+import defaultClasses from '@magento/venia-ui/lib/components/CheckoutPage/PaymentInformation/creditCard.module.css';
+import FormError from '@magento/venia-ui/lib/components/FormError';
+import GoogleReCaptcha from '@magento/venia-ui/lib/components/GoogleReCaptcha';
+
+const STEP_DESCRIPTIONS = [
+    { defaultMessage: 'Loading Payment', id: 'checkoutPage.step0' },
+    {
+        defaultMessage: 'Checking Credit Card Information',
+        id: 'checkoutPage.step1'
+    },
+    {
+        defaultMessage: 'Checking Credit Card Information',
+        id: 'checkoutPage.step2'
+    },
+    {
+        defaultMessage: 'Checking Credit Card Information',
+        id: 'checkoutPage.step3'
+    },
+    {
+        defaultMessage: 'Saved Credit Card Information Successfully',
+        id: 'checkoutPage.step4'
+    }
+];
 
 /**
- * The CheckMo component renders all information to handle checkmo payment.
- *
- * @param {String} props.payableTo shop owner name where you need to send.
- * @param {String} props.mailingAddress shop owner post address where you need to send.
- * @param {Boolean} props.shouldSubmit boolean value which represents if a payment nonce request has been submitted
- * @param {Function} props.onPaymentSuccess callback to invoke when the a payment nonce has been generated
- * @param {Function} props.onPaymentReady callback to invoke when the component is ready
- * @param {Function} props.onPaymentError callback to invoke when component throws an error
- * @param {Function} props.resetShouldSubmit callback to reset the shouldSubmit flag
+ * The initial view for the "Stripe Payments" payment method.
  */
-const CheckMo = props => {
-    const classes = useStyle(defaultClasses, props.classes);
+const CreditCard = props => {
+    const {
+        classes: propClasses,
+        onPaymentSuccess: onSuccess,
+        onPaymentReady: onReady,
+        onPaymentError: onError,
+        resetShouldSubmit,
+        shouldSubmit
+    } = props;
+    const { formatMessage } = useIntl();
 
-    const addressTemplate = str => (
-        <span key={str} className={classes.addressLine}>
-            {str} <br />
-        </span>
-    );
+    const classes = useStyle(defaultClasses, propClasses);
+
+    const talonProps = useCreditCard({
+        onSuccess,
+        onReady,
+        onError,
+        shouldSubmit,
+        resetShouldSubmit
+    });
 
     const {
-        payableTo,
-        mailingAddress,
-        onBillingAddressChangedError,
-        onBillingAddressChangedSuccess
-    } = useCheckmo(props);
+        errors,
+        shouldRequestPayment,
+        onPaymentError,
+        onPaymentSuccess,
+        onPaymentReady,
+        isBillingAddressSame,
+        isLoading,
+        /**
+         * `stepNumber` depicts the state of the process flow in credit card
+         * payment flow.
+         *
+         * `0` No call made yet
+         * `1` Billing address mutation intiated
+         * `2` Stripe paymentIntent requsted
+         * `3` Payment information mutation intiated
+         * `4` All mutations done
+         */
+        stepNumber,
+        initialValues,
+        shippingAddressCountry,
+        shouldTeardownDropin,
+        resetShouldTeardownDropin,
+        recaptchaWidgetProps
+    } = talonProps;
 
-    const formatAddress = mailingAddress
-        ? mailingAddress.split('\n').map(str => addressTemplate(str))
-        : props.mailingAddress.split('\n').map(str => addressTemplate(str));
+    const creditCardComponentClassName = isLoading
+        ? classes.credit_card_root_hidden
+        : classes.credit_card_root;
+
+    const billingAddressFieldsClassName = isBillingAddressSame
+        ? classes.billing_address_fields_root_hidden
+        : classes.billing_address_fields_root;
+
+    /**
+     * Instead of defining classes={root: classes.FIELD_NAME}
+     * we are using useMemo to only do it once (hopefully).
+     */
+    const fieldClasses = useMemo(() => {
+        return [
+            'first_name',
+            'last_name',
+            'country',
+            'street1',
+            'street2',
+            'city',
+            'region',
+            'postal_code',
+            'phone_number'
+        ].reduce((acc, fieldName) => {
+            acc[fieldName] = { root: classes[fieldName] };
+
+            return acc;
+        }, {});
+    }, [classes]);
+
+    /**
+     * These 2 functions are wrappers around the `isRequired` function
+     * of `formValidators`. They perform validations only if the
+     * billing address is different from shipping address.
+     *
+     * We write this function in `venia-ui` and not in the `peregrine` talon
+     * because it references `isRequired` which is a `venia-ui` function.
+     */
+    const isFieldRequired = useCallback((value, { isBillingAddressSame }) => {
+        if (isBillingAddressSame) {
+            /**
+             * Informed validator functions return `undefined` if
+             * validation is `true`
+             */
+            return undefined;
+        } else {
+            return isRequired(value);
+        }
+    }, []);
+
+    const stepTitle = STEP_DESCRIPTIONS[stepNumber].defaultMessage
+        ? formatMessage({
+              id: STEP_DESCRIPTIONS[stepNumber].id,
+              defaultMessage: STEP_DESCRIPTIONS[stepNumber].defaultMessage
+          })
+        : formatMessage({
+              id: 'checkoutPage.loadingPayment',
+              defaultMessage: 'Loading Payment'
+          });
+
+    const loadingIndicator = isLoading ? (
+        <LoadingIndicator>{stepTitle}</LoadingIndicator>
+    ) : null;
 
     return (
-        <div className={classes.root}>
-            <p className={classes.title}>
-                <FormattedMessage
-                    id={'checkMo.payableToTitle'}
-                    defaultMessage={'Account to make a payment:'}
+        <div className={classes.root} data-cy="CreditCard-root">
+            <div className={creditCardComponentClassName}>
+                <FormError
+                    allowErrorMessages
+                    classes={{ root: classes.formErrorContainer }}
+                    errors={Array.from(errors.values())}
                 />
-            </p>
-            <p className={classes.formatAddress}>
-                {payableTo ? payableTo : props.payableTo}
-            </p>
-            <p className={classes.mailingAddressTitle}>
-                <FormattedMessage
-                    id={'checkMo.mailingAddressTitle'}
-                    defaultMessage={'Send Payment to:'}
-                />
-            </p>
-            <p className={classes.formatAddress}>{formatAddress}</p>
-            <p className={classes.note}>
-                <FormattedMessage
-                    id={'checkMo.note'}
-                    defaultMessage={
-                        'Note: Your order will be shipped once the Transfer has been received and processed.'
-                    }
-                />
-            </p>
-            <BillingAddress
-                resetShouldSubmit={props.resetShouldSubmit}
-                shouldSubmit={props.shouldSubmit}
-                onBillingAddressChangedError={onBillingAddressChangedError}
-                onBillingAddressChangedSuccess={onBillingAddressChangedSuccess}
-            />
+                <div className={classes.dropin_root}>
+                    <StripeDropin
+                        onError={onPaymentError}
+                        onReady={onPaymentReady}
+                        onSuccess={onPaymentSuccess}
+                        shouldRequestPayment={shouldRequestPayment}
+                        shouldTeardownDropin={shouldTeardownDropin}
+                        resetShouldTeardownDropin={resetShouldTeardownDropin}
+                    />
+                </div>
+                <div
+                    data-cy="CreditCard-AddressCheck-root"
+                    className={classes.address_check}
+                >
+                    <Checkbox
+                        data-cy="PaymentInformation-billingAddressSame"
+                        field="isBillingAddressSame"
+                        label={formatMessage({
+                            id: 'checkoutPage.billingAddressSame',
+                            defaultMessage:
+                                'Billing address same as shipping address'
+                        })}
+                        initialValue={initialValues.isBillingAddressSame}
+                    />
+                </div>
+                <div
+                    data-cy="CreditCard-billingAddressFields"
+                    className={billingAddressFieldsClassName}
+                >
+                    <Field
+                        id="firstName"
+                        classes={fieldClasses.first_name}
+                        label={formatMessage({
+                            id: 'global.firstName',
+                            defaultMessage: 'First Name'
+                        })}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-firstname"
+                            id="firstName"
+                            field="firstName"
+                            validate={isFieldRequired}
+                            initialValue={initialValues.firstName}
+                        />
+                    </Field>
+                    <Field
+                        id="lastName"
+                        classes={fieldClasses.last_name}
+                        label={formatMessage({
+                            id: 'global.lastName',
+                            defaultMessage: 'Last Name'
+                        })}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-lastname"
+                            id="lastName"
+                            field="lastName"
+                            validate={isFieldRequired}
+                            initialValue={initialValues.lastName}
+                        />
+                    </Field>
+                    <Country
+                        data-cy="CreditCard-billingAddress-country"
+                        classes={fieldClasses.country}
+                        validate={isFieldRequired}
+                        initialValue={
+                            /**
+                             * If there is no initial value to start with
+                             * use the country from shipping address.
+                             */
+                            initialValues.country || shippingAddressCountry
+                        }
+                    />
+                    <Field
+                        id="street1"
+                        classes={fieldClasses.street1}
+                        label={formatMessage({
+                            id: 'global.streetAddress',
+                            defaultMessage: 'Street Address'
+                        })}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-street1"
+                            id="street1"
+                            field="street1"
+                            validate={isFieldRequired}
+                            initialValue={initialValues.street1}
+                        />
+                    </Field>
+                    <Field
+                        id="street2"
+                        classes={fieldClasses.street2}
+                        label={formatMessage({
+                            id: 'global.streetAddress2',
+                            defaultMessage: 'Street Address 2'
+                        })}
+                        optional={true}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-street2"
+                            id="street2"
+                            field="street2"
+                            initialValue={initialValues.street2}
+                        />
+                    </Field>
+                    <Field
+                        id="city"
+                        classes={fieldClasses.city}
+                        label={formatMessage({
+                            id: 'global.city',
+                            defaultMessage: 'City'
+                        })}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-city"
+                            id="city"
+                            field="city"
+                            validate={isFieldRequired}
+                            initialValue={initialValues.city}
+                        />
+                    </Field>
+                    <Region
+                        data-cy="CreditCard-billingAddress-region"
+                        classes={fieldClasses.region}
+                        initialValue={initialValues.region}
+                        validate={isFieldRequired}
+                        fieldInput={'region[label]'}
+                        fieldSelect={'region[region_id]'}
+                        optionValueKey={'id'}
+                    />
+                    <Postcode
+                        data-cy="CreditCard-billingAddress-postcode"
+                        classes={fieldClasses.postal_code}
+                        validate={isFieldRequired}
+                        initialValue={initialValues.postcode}
+                    />
+                    <Field
+                        id="phoneNumber"
+                        classes={fieldClasses.phone_number}
+                        label={formatMessage({
+                            id: 'global.phoneNumber',
+                            defaultMessage: 'Phone Number'
+                        })}
+                    >
+                        <TextInput
+                            data-cy="CreditCard-billingAddress-phoneNumber"
+                            id="phoneNumber"
+                            field="phoneNumber"
+                            validate={isFieldRequired}
+                            initialValue={initialValues.phoneNumber}
+                        />
+                    </Field>
+                </div>
+                <GoogleReCaptcha {...recaptchaWidgetProps} />
+            </div>
+            {loadingIndicator}
         </div>
     );
 };
 
-CheckMo.propTypes = {
-    classes: shape({ root: string }),
-    payableTo: string,
-    mailingAddress: string,
+export default CreditCard;
+
+CreditCard.propTypes = {
+    classes: shape({
+        root: string,
+        dropin_root: string,
+        billing_address_fields_root: string,
+        first_name: string,
+        last_name: string,
+        city: string,
+        region: string,
+        postal_code: string,
+        phone_number: string,
+        country: string,
+        street1: string,
+        street2: string,
+        address_check: string,
+        credit_card_root: string,
+        credit_card_root_hidden: string
+    }),
     shouldSubmit: bool.isRequired,
     onPaymentSuccess: func,
     onPaymentReady: func,
     onPaymentError: func,
     resetShouldSubmit: func.isRequired
 };
-
-CheckMo.defaultProps = {
-    payableTo: '',
-    mailingAddress: ''
-};
-
-export default CheckMo;
